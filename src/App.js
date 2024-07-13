c7392b0d68b2b71a70fd9e2b2b21fc7308034324
Gal Rabinovich <thegal9989@gmail.com>
2024-07-13 20:54:06 +0300

    Render App.js changes

--- src/App.js
+++ src/App.js
@@ -15,10 +15,22 @@ function App() {
         <PingAlertComponent />
         <div className="App-content">
           <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/homepage" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="https://foodiefriends.onrender.com/"
+              element={<Homepage />}
+            />
+            <Route
+              path="https://foodiefriends.onrender.com/homepage"
+              element={<Homepage />}
+            />
+            <Route
+              path="https://foodiefriends.onrender.com/about"
+              element={<About />}
+            />
+            <Route
+              path="https://foodiefriends.onrender.com/login"
+              element={<Login />}
+            />
           </Routes>
         </div>
       </div>
