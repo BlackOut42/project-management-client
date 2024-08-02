9c07109200e9716f9b5e998701a2ed4e5258fb02
Sergei <sergeiyakima1@gmail.com>
2024-08-02 15:03:20 +0300

    prevented access to personal area while not being logged in

--- src/PersonalArea.js
+++ src/PersonalArea.js
@@ -1,5 +1,6 @@
 // /src/PersonalArea.js
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "./authContext";
 import Sidebar from "./PersonalArea/Sidebar";
 import UserDetails from "./PersonalArea/UserDetails";
 import ChangePassword from "./PersonalArea/ChangePassword";
@@ -10,6 +11,7 @@ import Statistics from "./PersonalArea/Statistics";
 import "./styles/PersonalArea.css";
 
 const PersonalArea = () => {
+  const { authData } = useContext(AuthContext);
   const [selectedPanel, setSelectedPanel] = useState("details");
 
   const renderPanel = () => {
@@ -31,14 +33,21 @@ const PersonalArea = () => {
     }
   };
 
-  return (
-    <div className="personalarea-container">
-      <Sidebar onSelectPanel={setSelectedPanel} />
-      <div className="panel-container">
-        {renderPanel()}
+  if (authData) {
+    return (
+      <div className="personalarea-container">
+        <Sidebar onSelectPanel={setSelectedPanel} />
+        <div className="panel-container">
+          {renderPanel()}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else {
+    return (
+      <div className="error-message">You are unauthorized to view this page, please log into an existing account.</div>
+    );
+  }
 };
 
 export default PersonalArea;
